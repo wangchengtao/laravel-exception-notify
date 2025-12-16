87a4c8f078384169552a0d6a17c21b8d017cad98
wangchengtao <915129420@qq.com>
2025-12-16 11:23:12 +0800

    bug fixed

--- config/message.php
+++ config/message.php
@@ -7,14 +7,14 @@ return [
         'dingtalk' => [
             'driver' => Summer\ExceptionNotify\Channel\Dingtalk::class,
             'access_token' => 'your access token',
-            'type' => 'keyword', // keyword sign
+            'auth_type' => 'keyword', // keyword | sign
             'keyword' => 'your keyword', // When the type is keyword, this refers to the keyword added to the bot.
             'secret' => 'your secret', // When the type is sign, this refers to the secret adding the bot.
         ],
         'feishu' => [
             'driver' => Summer\ExceptionNotify\Channel\Feishu::class,
             'access_token' => 'your access token',
-            'type' => 'keyword', // keyword sign
+            'auth_type' => 'keyword', // keyword | sign
             'keyword' => 'your keyword',
             'secret' => 'your secret',
         ],
