0ac5b7428eb0634e5a13074f20d63a757aff2fee
wangchengtao <915129420@qq.com>
2025-12-16 19:32:54 +0800

    update message.php

--- config/message.php
+++ config/message.php
@@ -6,21 +6,21 @@ return [
     'channels' => [
         'dingtalk' => [
             'driver' => Summer\ExceptionNotify\Channel\Dingtalk::class,
-            'access_token' => 'your access token',
-            'auth_type' => 'keyword', // keyword | sign
-            'keyword' => 'your keyword', // When the type is keyword, this refers to the keyword added to the bot.
-            'secret' => 'your secret', // When the type is sign, this refers to the secret adding the bot.
+            'access_token' => env('DINGTALK_ACCESS_TOKEN', 'your access token'),
+            'auth_type' => env('DINGTALK_AUTH_TYPE', 'keyword'), // keyword | sign
+            'keyword' => env('DINGTALK_KEYWORD', 'your keyword'), // When the type is keyword, this refers to the keyword added to the bot.
+            'secret' => env('DINGTALK_SECRET', 'your secret'), // When the type is sign, this refers to the secret adding the bot.
         ],
         'feishu' => [
             'driver' => Summer\ExceptionNotify\Channel\Feishu::class,
-            'access_token' => 'your access token',
-            'auth_type' => 'keyword', // keyword | sign
-            'keyword' => 'your keyword',
-            'secret' => 'your secret',
+            'access_token' => env('FEISHU_ACCESS_TOKEN', 'your access token'),
+            'auth_type' => env('FEISHU_KEYWORD', 'keyword'), // keyword | sign
+            'keyword' => env('FEISHU_KEYWORD', 'your keyword'),
+            'secret' => env('FEISHU_SECRET', 'your secret'),
         ],
         'wechat' => [
             // 'driver' => Summer\ExceptionNotify\Channel\Wechat::class,
-            'access_token' => 'your access token',
+            'access_token' => env('WECHAT_ACCESS_TOKEN', 'your access token'),
         ],
     ],
 ];
