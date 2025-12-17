faeee8b69a21ec1e590d038ac1f790ee9d559966
wangchengtao <915129420@qq.com>
2025-12-17 22:58:59 +0800

    update message.php

--- config/message.php
+++ config/message.php
@@ -5,16 +5,38 @@ return [
     'channels' => [
         'dingtalk' => [
             'driver' => Summer\ExceptionNotify\Channel\Dingtalk::class,
+
+            /*
+             * 自定义机器人调用接口的凭证
+             * 自定义机器人安装后webhook地址中的access_token值。详情参考获取自定义机器人 Webhook 地址。
+             */
             'access_token' => env('DINGTALK_ACCESS_TOKEN', 'your access token'),
-            'auth_type' => env('DINGTALK_AUTH_TYPE', 'keyword'), // keyword | sign
-            'keyword' => env('DINGTALK_KEYWORD', 'your keyword'), // When the type is keyword, this refers to the keyword added to the bot.
-            'secret' => env('DINGTALK_SECRET', 'your secret'), // When the type is sign, this refers to the secret adding the bot.
-            'at' => explode(',', env('DINGTALK_AT', '')), // At who
+
+            /*
+             * 安全设置
+             * 添加机器人时选择的安全设置类型, 支持自定义关键词(keyword) 和 加签(sign)
+             */
+            'auth_type' => env('DINGTALK_AUTH_TYPE', 'keyword'),
+
+            /*
+             * When the auth type is keyword, this refers to the keyword added to the bot.
+             */
+            'keyword' => env('DINGTALK_KEYWORD', 'your keyword'),
+
+            /*
+             * When the auth type is sign, this refers to the secret adding the bot.
+             */
+            'secret' => env('DINGTALK_SECRET', 'your secret'),
+
+            /*
+             * 被@的群成员手机号
+             */
+            'at' => explode(',', env('DINGTALK_AT', '')),
         ],
         'feishu' => [
             'driver' => Summer\ExceptionNotify\Channel\Feishu::class,
             'access_token' => env('FEISHU_ACCESS_TOKEN', 'your access token'),
-            'auth_type' => env('FEISHU_KEYWORD', 'keyword'), // keyword | sign
+            'auth_type' => env('FEISHU_KEYWORD', 'keyword'),
             'keyword' => env('FEISHU_KEYWORD', 'your keyword'),
             'secret' => env('FEISHU_SECRET', 'your secret'),
         ],
